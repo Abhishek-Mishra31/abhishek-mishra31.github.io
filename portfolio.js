744b68138ca66024ded184ba37c5f831fd9cbbac
Abhishek <abhishekebelaganj0609@gmail.com>
2026-03-13 10:02:56 +0530

    Enhanced Portfolio

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,12 @@
 var typed = new Typed(".Multiple-Text", {
   strings: [
-    "Full-Stack Developer",
-    "Programmer",
-    "UX Designer",
-    "Tech Enthusiast",
-    "Open-Source Contributor",
+    "Software Developer",
+    "Full Stack Engineer",
+    "Open Source Enthusiast",
     "Problem Solver",
+    "Tech Explorer",
+    "Clean Code Advocate",
+    "Debugging Ninja",
   ],
   typeSpeed: 80,
   backSpeed: 80,
